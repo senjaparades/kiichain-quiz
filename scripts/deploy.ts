1503c6959cf78e3455b95c7d12ca0d9cfa66f9f1
senjaparades <emailkamu@example.com>
2025-07-26 05:37:23 +0700

    fix: update deploy script to use ABI from lib/abi

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,6 +1,7 @@
 import { ethers } from "ethers";
 import * as dotenv from "dotenv";
-import KiiQuizJson from "../artifacts/contracts/KiiQuiz.sol/KiiQuiz.json";
+import KiiQuizJson from "../lib/abi/KiiQuiz.json";
+
 
 dotenv.config();
 
